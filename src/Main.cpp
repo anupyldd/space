c110ee6082a1bed233b8cab4604758f4d1864ec4
Dmitry Anupyld <dmia@getcrm.ru>
2024-08-10 11:18:12 +0300

    add utf8 char & str

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,14 @@
-
+﻿#include "Windows.h"
 #include <iostream>
+#include "base/Types.h"
+#include <format>
 
+using namespace spc;
 int main(int argc, char** argv)
 {
-    std::cout << "testest\n";
+    String s;
+    con::Input(s);
+    con::PrintN(s);
+    //std::cout << "\x1b[95mMagenta\x1b[0m";
+    std::cin.get();
 }

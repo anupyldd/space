b1c40f1350d4d92c5eedb5a0cc3385ec046ec0a4
Dmitry Anupyld <dmia@getcrm.ru>
2024-08-10 22:47:54 +0300

    base file struct

--- src/core/Defines.h
+++ src/core/Defines.h
@@ -3,9 +3,11 @@
 #if defined(_WIN64) || defined(_WIN32)
     
     #define WINDOWS_PLATFORM 1
+    #define PLATFORM_TYPE(win, unix) win    // for when different type is needed on different platforms
 
 #elif defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
 
     #define WINDOWS_PLATFORM 0
+    #define PLATFORM_TYPE(win, unix) unix    // for when different type is needed on different platforms
 
 #endif

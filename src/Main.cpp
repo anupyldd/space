3965ee2084ed8c37919a283bf47e53eaa06a99fd
Dmitry Anupyld <dmia@getcrm.ru>
2024-08-10 14:10:17 +0300

    enable ansi codes

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,13 @@
 ﻿#include "Windows.h"
 #include <iostream>
 #include "base/Types.h"
+#include "base/Console.h"
 #include <format>
 
 using namespace spc;
 int main(int argc, char** argv)
 {
-    String s;
-    con::Input(s);
-    con::PrintN(s);
-    //std::cout << "\x1b[95mMagenta\x1b[0m";
+    con::Init();
+    std::cout << "\x1b[95mMagenta\x1b[0m";
     std::cin.get();
 }
--- src/base/Console.h
+++ src/base/Console.h
@@ -1,4 +1,24 @@
 
+
+// API ---------------------------------
+
+namespace spc
+{
+    namespace con
+    {
+        inline void Init(); // must be called before any printing
+
+        inline std::wstring Utf8ToUtf16(const String& str8);
+        inline String Utf16ToUtf8(const std::wstring& str16);
+
+        inline void Print(const String& str);
+        inline void PrintN(const String& str);
+        inline void Input(String& str);
+    }
+}
+
+// -------------------------------------
+
 namespace spc
 {
 
@@ -7,29 +27,76 @@ namespace spc
 
     #include "WinDef.h"
 
+    #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+    #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+    #endif
+
+    static HANDLE stdoutHandle;
+    static DWORD outModeInit;
+
     namespace con
     {
-        inline std::wstring Utf8ToUtf16(const String& str8) {
+        // internal functions
+        namespace itrn
+        {
+            inline void EnableANSI()
+        {
+            DWORD outMode = 0;
+            stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            if (stdoutHandle == INVALID_HANDLE_VALUE)
+            {
+                exit(GetLastError());
+            }
+
+            if (!GetConsoleMode(stdoutHandle, &outMode))
+            {
+                exit(GetLastError());
+            }
+
+            outModeInit = outMode;
+            outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+            if (!SetConsoleMode(stdoutHandle, outMode))
+            {
+                exit(GetLastError());
+            }
+        }
+        }
+        // -----------------
+        inline void Init()
+        {
+            itrn::EnableANSI();
+            system("chcp 65001");
+            //SetConsoleOutputCP(65001);
+        }
+
+        inline std::wstring Utf8ToUtf16(const String& str8) 
+        {
             int sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, str8.c_str(), (int)str8.size(), NULL, 0);
             std::wstring str16(sizeNeeded, 0);
             MultiByteToWideChar(CP_UTF8, 0, str8.c_str(), (int)str8.size(), &str16[0], sizeNeeded);
             return str16;
         }
 
-        inline String Utf16ToUtf8(const std::wstring& str16) {
+        inline String Utf16ToUtf8(const std::wstring& str16) 
+        {
             int sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str16.c_str(), (int)str16.size(), NULL, 0, NULL, NULL);
             String str8(sizeNeeded, 0);
             WideCharToMultiByte(CP_UTF8, 0, str16.c_str(), (int)str16.size(), &str8[0], sizeNeeded, NULL, NULL);
             return str8;
         }
 
-        inline void Print(const String& str) {
+        inline void Print(const String& str) 
+        {
             std::wcout << Utf8ToUtf16(str);
         }
-        inline void PrintN(const String& str) {
+        inline void PrintN(const String& str) 
+        {
             std::wcout << Utf8ToUtf16(str) << L'\n';
         }
-        inline void Input(String& str) {
+        inline void Input(String& str) 
+        {
             std::wstring wstr;
             std::wcin >> wstr;
             str = Utf16ToUtf8(wstr);
@@ -41,6 +108,8 @@ namespace spc
 
     namespace con
     {
+        inline void Init() {}
+
         inline void Print(const String& str) { std::cout << str; }
         inline void PrintN(const String& str) { std::cout << str << '\n'; }
         inline void Input(String& str) { std::cin >> str; }
@@ -52,5 +121,4 @@ namespace spc
 
 #endif
 
-}
 }
